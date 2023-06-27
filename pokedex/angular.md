419f898d2e8103c857da9719501ac271d52889c1
sy-alx <84022109+sy-alx@users.noreply.github.com>
2023-06-27 10:51:08 +0200

    commencement de modification

---
+++ pokedex/angular.md
@@ -0,0 +1,23 @@
+cette directive permet d'a&gir sur le style d'un élément : 
+``` html
+<p [ngStyle]="{color: color}"> Bulbizar <p>
+
+```
+
+Cette directive permet de modififier dynamique un classe sur un element
+
+``` html
+
+<div [class.someClass]="isStyleWithSomeClass()"> Ng Class ! </div>
+
+```
+
+## Résumé
+
+On peut utiliser tout un tas de symbole afin d'intéragir avec le template : 
+    {{}} pour afficher le contenue de variable ng ou directive qui comment par * 
+    [] pour le binding de proprièetes 
+    création de varible locales avec #
+    usage des selecteurs pour imbriquer les tempaltes de composants
+    
+    
--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdTypeaheadFocus } from "./components/card/typehead-focus";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CardComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        NgbdTypeaheadFocus
+    ]
 })
 export class AppModule { }
